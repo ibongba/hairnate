e786c5efaea8eaa3fdcb8f6fd08fe9f5ef63bc6c
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2020-08-22 04:03:22 +0700

    update

--- service/core.php
+++ service/core.php
@@ -12,30 +12,93 @@
     $conn->exec("SET CHARACTER SET utf8");
 
     if(isset($_POST['action']) && $_POST['action'] == 'create'){
+        $img = '';
+        if(isset($_FILES['files'])){
+            $total = count($_FILES['files']['name']);
 
-        $sql = "INSERT INTO `users`(`username`, `password`, `phone`, `email`, `address`) VALUES ('".$_POST['username']."','".md5($_POST['password'])."','".$_POST['phone']."','".$_POST['email']."','".$_POST['address']."')";
+            for( $i=0 ; $i < $total ; $i++ ) {
+                $tmpFilePath = $_FILES['files']['tmp_name'][$i];
+                if ($tmpFilePath != ""){
+                    $newFilePath = "../upload/". time() . $_FILES['files']['name'][$i];
+                    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+                        $img = "upload/" .time() . $_FILES['files']['name'][$i];;
+                    }
+                }
+            }
+        }
+
+        $sql = "SELECT * FROM `partner` WHERE `fk_user_id` = '".$_POST['user_id']."'";
+        $rs = getpdo($conn,$sql);
+
+        if($rs){
+            $sql = "INSERT INTO `hairstyle`(`id_partner`, `name`, `description`, `price`, `image`) VALUES ('".$rs[0]['id']."','".$_POST['name']."','".$_POST['description']."','".$_POST['price']."','".$img."')";
+            $rs = getpdo($conn,$sql);
+            if($rs){
+                if(isset($_POST['promotion'])){
+
+                    $lastid = $conn->lastInsertId();
+
+                    $sql = "UPDATE `users` SET `type` = 2 , `surname` = '".$_POST['surname']."' WHERE `id` = '".$lastid."'";
+                    $rs = getpdo($conn,$sql);
+
+                    $sql = "INSERT INTO `partner`(`name`, `biz_email`, `location`, `biz_type`, `fk_user_id`) VALUES ('".$_POST['name']."','".$_POST['biz_email']."','".$_POST['location']."','".$_POST['biz_type']."','".$lastid."')";
+                    $rs = getpdo($conn,$sql);
+                    if($rs){
+                        $res = array("code" => 200, "result" => $rs);
+                        echo json_encode($res);
+                        return ;
+                    }
+                }
+                else{
+                    $res = array("code" => 200, "result" => $rs);
+                    echo json_encode($res);
+                    return ;
+                }
+            }
+        } 
+    }else if(isset($_POST['action']) && $_POST['action'] == 'getservice'){
+
+        $sql = "SELECT * FROM `partner` WHERE `fk_user_id` = '".$_POST['user_id']."'";
+            // echo $sql;
+        $rs = getpdo($conn,$sql);
+
+        if($rs){
+            $sql = "SELECT * FROM `hairstyle` WHERE `id_partner` = '".$rs[0]['id']."' and `type` = 1";
+            $rs = getpdo($conn,$sql);
+            if($rs){
+                $res = array("code" => 200, "result" => $rs);
+                echo json_encode($res);
+                return ;
+            }
+        }
+    }else if(isset($_POST['action']) && $_POST['action'] == 'edit'){
+
+        $img = '';
+        if(isset($_FILES['files'])){
+            $total = count($_FILES['files']['name']);
+
+            for( $i=0 ; $i < $total ; $i++ ) {
+                $tmpFilePath = $_FILES['files']['tmp_name'][$i];
+                if ($tmpFilePath != ""){
+                    $newFilePath = "../upload/". time() . $_FILES['files']['name'][$i];
+                    if(move_uploaded_file($tmpFilePath, $newFilePath)) {
+                        $img = "upload/" .time() . $_FILES['files']['name'][$i];;
+                    }
+                }
+            }
+        }
+
+        $sql = "UPDATE `hairstyle` SET `name`='".$_POST['name']."',`description`='".$_POST['description']."',`price`='".$_POST['price']."'";
+        if(!empty($img)) {
+            $sql .= ",`image` = '".$img."'";
+        }
+        $sql .= " WHERE `id_hairstyle` = '".$_POST['id_hairstyle']."'";
+
         $rs = getpdo($conn,$sql);
         if($rs){
-        	if(isset($_POST['partner'])){
-
-        		$lastid = $conn->lastInsertId();
-
-        		$sql = "UPDATE `users` SET `type` = 2 , `surname` = '".$_POST['surname']."' WHERE `id` = '".$lastid."'";
-        		$rs = getpdo($conn,$sql);
-
-        		$sql = "INSERT INTO `partner`(`name`, `biz_email`, `location`, `biz_type`, `fk_user_id`) VALUES ('".$_POST['name']."','".$_POST['biz_email']."','".$_POST['location']."','".$_POST['biz_type']."','".$lastid."')";
-        		$rs = getpdo($conn,$sql);
-        		if($rs){
-					$res = array("code" => 200, "result" => $rs);
-		        	echo json_encode($res);
-		            return ;
-				}
-        	}
-			else{
-				$res = array("code" => 200, "result" => $rs);
-	        	echo json_encode($res);
-	            return ;
-			}
+            $res = array("code" => 200, "result" => $rs);
+            echo json_encode($res);
+            return ;
         }
     }
 
